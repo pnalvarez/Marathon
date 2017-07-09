3896a90d99f89c7ad649abe87eea0df1f58034bc
pnalvarez <pedronalvarez@Pedros-MacBook-Pro.local>
2017-07-09 17:22:20 -0300

    Adding 1193

---
+++ 1193/1193/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1193
+//
+//  Created by Pedro Neves Alvarez on 7/9/17.
+//  Copyright © 2017 Pedro Neves Alvarez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
