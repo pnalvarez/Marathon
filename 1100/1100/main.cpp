e697a28d2cccb094a30e012ac5316f0ab776b957
pnalvarez <pedronalvarez@Pedros-MacBook-Pro.local>
2017-07-10 00:10:53 -0300

    Adding 1100

---
+++ 1100/1100/main.cpp
@@ -0,0 +1,131 @@
+//
+//  main.cpp
+//  1100
+//
+//  Created by Pedro Neves Alvarez on 7/10/17.
+//  Copyright © 2017 Pedro Neves Alvarez. All rights reserved.
+//
+
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+map<string,vector<string> > graph;
+map<string, bool> visited;
+map<string, int> distances;
+queue<string> q;
+
+void create_chess_graph() {
+    int i, j;
+    string letters = "-abcdefgh";
+    string numbers = "-12345678";
+    string li, nj;
+    
+    for (i = 1; i <= 8; i++) {
+        for (j = 1; j <= 8; j++) {
+            li = letters[i];
+            nj = numbers[j];
+            
+            if (i + 2 <= 8 && j + 1 <= 8) {
+                graph[li + nj].push_back(
+                                         letters.substr(i + 2, 1) + numbers.substr(j + 1, 1));
+            }
+            
+            if (i + 2 <= 8 && j - 1 >= 1) {
+                graph[li + nj].push_back(
+                                         letters.substr(i + 2, 1) + numbers.substr(j - 1, 1));
+            }
+            
+            if (i - 2 >= 1 && j + 1 <= 8) {
+                graph[li + nj].push_back(
+                                         letters.substr(i - 2, 1) + numbers.substr(j + 1, 1));
+            }
+            
+            if (i - 2 >= 1 && j - 1 >= 1) {
+                graph[li + nj].push_back(
+                                         letters.substr(i - 2, 1) + numbers.substr(j - 1, 1));
+            }
+            
+            if (i + 1 <= 8 && j + 2 <= 8) {
+                graph[li + nj].push_back(
+                                         letters.substr(i + 1, 1) + numbers.substr(j + 2, 1));
+            }
+            
+            if (i + 1 <= 8 && j - 2 >= 1) {
+                graph[li + nj].push_back(
+                                         letters.substr(i + 1, 1) + numbers.substr(j - 2, 1));
+            }
+            
+            if (i - 1 >= 1 && j + 2 <= 8) {
+                graph[li + nj].push_back(
+                                         letters.substr(i - 1, 1) + numbers.substr(j + 2, 1));
+            }
+            
+            if (i - 1 >= 1 && j - 2 >= 1) {
+                graph[li + nj].push_back(
+                                         letters.substr(i - 1, 1) + numbers.substr(j - 2, 1));
+            }
+        }
+    }
+}
+
+void unvisit() {
+    int i, j;
+    string letters = "-abcdefgh";
+    string numbers = "-12345678";
+    string li, nj;
+    
+    for (i = 1; i <= 8; i++) {
+        for (j = 1; j <= 8; j++) {
+            li = letters[i];
+            nj = numbers[j];
+            
+            visited[li + nj] = false;
+        }
+    }
+}
+
+int bfs(string start, string goal) {
+    vector<string>::iterator it;
+    
+    unvisit();
+    
+    visited[start] = true;
+    q.push(start);
+    distances[start] = 0;
+    
+    while (!q.empty()) {
+        std::string current = q.front();
+        q.pop();
+        
+        for (it = graph[current].begin(); it != graph[current].end(); it++) {
+            if (!visited[*it]) {
+                visited[*it] = true;
+                q.push(*it);
+                distances[*it] = distances[current] + 1;
+            }
+        }
+    }
+    
+    return distances[goal];
+}
+
+int main() {
+    string from, to;
+    
+    create_chess_graph();
+    
+    while (std::cin >> from >> to) {
+        cout << "To get from " << from;
+        cout << " to " << to << " takes " << bfs(from, to);
+        cout << " knight moves." << endl;
+        
+        distances.clear();
+    }
+    
+    return 0;
+}
