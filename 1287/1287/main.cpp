7f2b81cc9337d74da773bfcec1772dae737dc115
pnalvarez <pedronalvarez@Pedros-MacBook-Pro.local>
2017-07-10 00:35:38 -0300

    Adding 1287

---
+++ 1287/1287/main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//  1287
+//
+//  Created by Pedro Neves Alvarez on 7/10/17.
+//  Copyright © 2017 Pedro Neves Alvarez. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+int main() {
+    char c;
+    long long s = 0;
+    bool got_any_number = false,
+    got_invalid_char = false,
+    got_overflow = false;
+    std::string valid_chars = "0123456789lOo";
+    std::string ignored_chars = ", ";
+    
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            if (got_any_number && !got_overflow && !got_invalid_char) {
+                std::cout << s << std::endl;
+            } else {
+                std::cout << "error" << std::endl;
+            }
+            
+            s = 0;
+            got_any_number = false;
+            got_invalid_char = false;
+            got_overflow = false;
+            
+            continue;
+        }
+        
+        if (valid_chars.find(c) != std::string::npos) {
+            if (c == 'o' || c == 'O') c = '0';
+            if (c == 'l') c = '1';
+            
+            s = (s * 10) + c - '0';
+            if (s > 2147483647) {
+                got_overflow = true;
+                s = 0;
+            }
+            got_any_number = true;
+        } else if (ignored_chars.find(c) == std::string::npos) {
+            got_invalid_char = true;
+        }
+    }
+    return 0;
+}
